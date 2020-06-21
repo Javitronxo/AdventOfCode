70611649cd8501d193e532204ad649e6594ca040
Javier Espinosa <javier_espinosa@apple.com>
2020-06-21 12:35:39 -0700

    2015: Add solution for day 10

---
+++ 2015/day_10.py
@@ -0,0 +1,27 @@
+def look_and_say(input_value: str) -> str:
+    prev_char = input_value[0]
+    output_str = ''
+    multiplier = 0
+    for char in input_value:
+        if char == prev_char:
+            multiplier += 1
+        else:
+            output_str += str(multiplier) + prev_char
+            prev_char = char
+            multiplier = 1
+    output_str += str(multiplier) + prev_char
+    return output_str
+
+
+def main():
+    input_str = open('day_10_input.txt').read()
+    iterations = 50
+    for i in range(iterations):
+        input_str = look_and_say(input_str)
+        if i == (40 - 1):  # Part 1: Result for 40 iterations
+            print('The length of the result is %d' % len(input_str))
+    print('The length of the result is %d' % len(input_str))
+
+
+if __name__ == '__main__':
+    main()
