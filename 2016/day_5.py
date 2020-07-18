ddf9532ff7b0c384dc1991bff754ded9a62e79fd
Javier Espinosa <j.espinosa90@gmail.com>
2020-07-18 14:40:37 -0700

    2016: Add solution for day 5

---
+++ 2016/day_5.py
@@ -0,0 +1,33 @@
+from hashlib import md5
+
+
+def get_md5_hash(input_str: str) -> str:
+    return md5(bytes(input_str, encoding='utf-8')).hexdigest()
+
+
+def main():
+    puzzle_input = open('day_5_input.txt').read()
+
+    password_1 = str()
+    password_2 = [None] * 8
+    i = 0
+    while len(password_1) < 8 or None in password_2:
+        candidate_hash = get_md5_hash(puzzle_input + str(i))
+        if candidate_hash.startswith('00000'):
+            # Check password for Part 1
+            if len(password_1) < 8:
+                password_1 += candidate_hash[5]
+            # Check password for Part 2
+            try:
+                if password_2[int(candidate_hash[5])] is None:
+                    password_2[int(candidate_hash[5])] = candidate_hash[6]
+            except (ValueError, IndexError):
+                pass
+        i += 1
+
+    print(f"Part 1: The password is {password_1}")
+    print(f"Part 2: The password is {''.join(password_2)}")
+
+
+if __name__ == '__main__':
+    main()
