866c765237d47ca8ddff291f4ac8a28f2146885b
Javier Espinosa <j.espinosa90@gmail.com>
2021-01-05 09:14:04 -0800

    2016: Separate assembunny code

--- 2016/day_12.py
+++ 2016/day_12.py
@@ -1,56 +1,12 @@
-class Register:
-    def __init__(self, value: int = 0):
-        self.value = value
-
-
-class Computer:
-    def __init__(self, offset_a: int = 0, offset_b: int = 0, offset_c: int = 0, offset_d: int = 0):
-        self.registers = {
-            "a": Register(offset_a),
-            "b": Register(offset_b),
-            "c": Register(offset_c),
-            "d": Register(offset_d),
-        }
-
-    def execute(self, instruction: str) -> int:
-        """
-        Execute the instruction and return the offset for the next one
-        """
-        parts = instruction.strip().split()
-        if parts[0] == "cpy":
-            try:
-                self.registers[parts[2]].value = self.registers[parts[1]].value
-            except KeyError:
-                self.registers[parts[2]].value = int(parts[1])
-        elif parts[0] == "inc":
-            self.registers[parts[1]].value += 1
-        elif parts[0] == "dec":
-            self.registers[parts[1]].value -= 1
-        elif parts[0] == "jnz":
-            try:
-                x = self.registers[parts[1]].value
-            except KeyError:
-                x = int(parts[1])
-            if x != 0:
-                return int(parts[2])
-        return 1
+from lib.assembunny_2016 import run_program
 
 
 def main():
     with open("day_12_input.txt") as f_in:
         instructions = f_in.readlines()
 
-    computer = Computer()
-    i = 0
-    while i < len(instructions):
-        i += computer.execute(instructions[i])
-    print(f"Part 1: Value in register A is: {computer.registers['a'].value}")
-
-    computer = Computer(offset_c=1)
-    i = 0
-    while i < len(instructions):
-        i += computer.execute(instructions[i])
-    print(f"Part 2: Value in register A is: {computer.registers['a'].value}")
+    print(f"Part 1: {run_program(instructions)}")
+    print(f"Part 2: {run_program(instructions, offset_c=1)}")
 
 
 if __name__ == "__main__":
