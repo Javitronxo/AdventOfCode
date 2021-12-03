72d1a08034107df4dc1d738ba630d51dae5c1759
Javier Espinosa <j.espinosa90@gmail.com>
2021-12-02 21:53:29 -0800

    2021: Add solution for day 2

---
+++ 2021/day_2.py
@@ -0,0 +1,30 @@
+def main():
+    position = [0, 0]
+    with open("day_2_input.txt") as f:
+        for line in f.readlines():
+            cmd, unit = line.split()
+            if cmd == "forward":
+                position[0] += int(unit)
+            elif cmd == "down":
+                position[1] += int(unit)
+            elif cmd == "up":
+                position[1] -= int(unit)
+    print(f"Part 1: {position[0] * position[1]}")
+
+    position = [0, 0]
+    aim = 0
+    with open("day_2_input.txt") as f:
+        for line in f.readlines():
+            cmd, unit = line.split()
+            if cmd == "forward":
+                position[0] += int(unit)
+                position[1] += (aim * int(unit))
+            elif cmd == "down":
+                aim += int(unit)
+            elif cmd == "up":
+                aim -= int(unit)
+    print(f"Part 2: {position[0] * position[1]}")
+
+
+if __name__ == '__main__':
+    main()
