4ce3d5c2f13d87e9b24973bf891845044c903b05
Javier Espinosa <j.espinosa90@gmail.com>
2021-12-06 22:23:49 -0800

    2021: Optimize solution for day 7

--- 2021/day_7.py
+++ 2021/day_7.py
@@ -2,11 +2,17 @@ def main():
     with open("day_7_input.txt") as f:
         puzzle_input = [int(x) for x in f.read().split(",")]
 
+    puzzle_input.sort()
     median = puzzle_input[len(puzzle_input) // 2]
-    mean = int(sum(puzzle_input) / len(puzzle_input))
+    fuel_one = sum(abs(n - median) for n in puzzle_input)
+    print(f"Part 1: {fuel_one}")
 
-    print(f"Part 1: {sum(abs(n - median) for n in puzzle_input)}")
-    print(f"Part 2: {sum(sum(i for i in range(1, abs(n - mean) + 1)) for n in puzzle_input)}")
+    mean = int(sum(puzzle_input) / len(puzzle_input))
+    fuel_two = sum(
+        (x := abs(n - mean)) * (x + 1) // 2
+        for n in puzzle_input
+    )
+    print(f"Part 2: {fuel_two}")
 
 
 if __name__ == "__main__":
