a5563d55ad7edd97b1442246f0dcb5d487dfe697
Javier Espinosa <me@javierespinosa.dev>
2023-12-02 10:31:33 -0800

    2023: Add solution for day 1

---
+++ 2023/day_1.py
@@ -0,0 +1,26 @@
+mapping = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+
+
+def main():
+    with open("day_1_input.txt") as f:
+        values_one = list()
+        values_two = list()
+        for line in f.readlines():
+            ints_one = list()
+            ints_two = list()
+            for i, char in enumerate(stripped_line := line.strip()):
+                try:
+                    ints_one.append(int(char))
+                    ints_two.append(int(char))
+                except ValueError:
+                    for key, value in mapping.items():
+                        if key == "".join(stripped_line[i:i + len(key)]):
+                            ints_two.append(value)
+            values_one.append(int(f"{ints_one[0]}{ints_one[-1]}"))
+            values_two.append(int(f"{ints_two[0]}{ints_two[-1]}"))
+    print(f"Part 1: {sum(values_one)}")
+    print(f"Part 2: {sum(values_two)}")
+
+
+if __name__ == "__main__":
+    main()
