de1b5bf84b635ffa5ac1c402d549461b43f9aa6e
Javier Espinosa <me@javierespinosa.dev>
2023-12-05 22:06:19 -0800

    2023: Correct typo for solution day 6

--- 2023/day_6.py
+++ 2023/day_6.py
@@ -12,13 +12,13 @@ def main():
 
     part_one = list()
     times_part_one = [int(i) for i in re.findall(r"\d+", lines[0])]
-    distances_part_two = [int(i) for i in re.findall(r"\d+", lines[1])]
-    for time, distance in zip(times_part_one, distances_part_two):
+    distances_part_one = [int(i) for i in re.findall(r"\d+", lines[1])]
+    for time, distance in zip(times_part_one, distances_part_one):
         part_one.append(ways_to_win(time, distance))
     print(f"Part 1: {reduce(lambda x, y: x*y, part_one)}")
 
     time_part_two = int("".join([str(i) for i in times_part_one]))
-    distance_part_two = int("".join([str(i) for i in distances_part_two]))
+    distance_part_two = int("".join([str(i) for i in distances_part_one]))
     print(f"Part 2: {ways_to_win(time_part_two, distance_part_two)}")
 
 
